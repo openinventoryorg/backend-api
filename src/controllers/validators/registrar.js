4733af618bb04d4fb2b629837fc8e5d41cd4df43
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-03-28 02:16:36 +0530

    Added end point to register multiple emails

--- src/controllers/validators/registrar.js
+++ src/controllers/validators/registrar.js
@@ -5,6 +5,11 @@ const RegistrationTokenGenerationRequest = Joi.object().keys({
     role: Joi.string().uuid().required(),
 });
 
+const RegistrationTokenListRequest = Joi.object().keys({
+    emails: Joi.array().min(1).items(Joi.string().email()).required(),
+    role: Joi.string().uuid().required(),
+});
+
 const EmailBasedRequest = Joi.object().keys({
     email: Joi.string().email().required(),
 });
@@ -13,4 +18,5 @@ const EmailBasedRequest = Joi.object().keys({
 module.exports = {
     RegistrationTokenGenerationRequest,
     EmailBasedRequest,
+    RegistrationTokenListRequest,
 };
