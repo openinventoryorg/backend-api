981fc268c1a8c1d8d6f7b8294316433db1130dae
dylan96dashintha <dashinthadilan.17@cse.mrt.ac.lk>
2020-04-04 00:04:18 +0530

    userlist

--- src/controllers/manageusers.js
+++ src/controllers/manageusers.js
@@ -1,13 +1,12 @@
 const ListService = require('../services/list');
 
 class ManageUserController {
-    static async ListUsers(req,res,next) {
+    static async ListUsers(req, res, next) {
         try {
             const users = await ListService.ListUsers();
             res.status(200).send(users);
         } catch (error) {
             next(error);
-            
         }
     }
 }
--- src/routes/manageusers.js
+++ src/routes/manageusers.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const ManageUsersController = require('../controllers/manageusers');
 
-router.get('/list', ManageUsersController.ListUsers );
+router.get('/list', ManageUsersController.ListUsers);
 
 module.exports = router;
