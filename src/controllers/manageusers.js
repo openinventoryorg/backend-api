1aa6e1074aa6c314a2846a67d41acae7ee4ef41d
dylan96dashintha <dashinthadilan.17@cse.mrt.ac.lk>
2020-04-03 23:04:11 +0530

    user list

---
+++ src/controllers/manageusers.js
@@ -0,0 +1,15 @@
+const ListService = require('../services/list');
+
+class ManageUserController {
+    static async ListUsers(req,res,next) {
+        try {
+            const users = await ListService.ListUsers();
+            res.status(200).send(users);
+        } catch (error) {
+            next(error);
+            
+        }
+    }
+}
+
+module.exports = ManageUserController;
