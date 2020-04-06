e97f7833d2e2de59531003a7a7702802adba57ae
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-04-06 20:08:20 +0530

    Corrected image in lab manage validation

--- src/controllers/validators/managelabs.js
+++ src/controllers/validators/managelabs.js
@@ -3,14 +3,16 @@ const Joi = require('@hapi/joi');
 const CreateLab = Joi.object().keys({
     title: Joi.string().required(),
     subtitle: Joi.string().required(),
-    image: Joi.string().max(1023),
+    image: Joi.string().max(1023).required()
+        .allow(null),
 });
 
 const CreateLabQuery = Joi.object().keys({
     id: Joi.string().uuid().required(),
     title: Joi.string().required(),
     subtitle: Joi.string().required(),
-    image: Joi.string().max(1023).uri(),
+    image: Joi.string().max(1023).required()
+        .allow(null),
 });
 
 
