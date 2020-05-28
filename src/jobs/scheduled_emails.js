bfc47c2df0f50f311ec572ddd16174eddbb0e1eb
Gayal <g.c.dassanayake@gmail.com>
2020-05-28 12:48:39 +0530

    temp

---
+++ src/jobs/scheduled_emails.js
@@ -0,0 +1,26 @@
+// const { sendMail } = require('../emails');
+// const config = require('../config');
+// const { getDatabase } = require('../helpers/get_database');
+
+// const remindItemRequestDeadline = async () => {
+//     const database = await getDatabase();
+//     const
+// };
+
+// const sendRemindMail = () => {
+//     sendMail({
+//         from: config.mail.sender,
+//         to: recipient.email,
+//         subject: 'Item Request Link - Open Inventory',
+//         template: 'supervisor_invite',
+//         context: {
+//         // firstName: supervisor.firstName,
+//         // lastName: supervisor.lastName,
+//         // labTitle: lab.title,
+//         // email: supervisor.email,
+//         // link: `${config.site.verifyToken}/${supervisorToken}`,
+//         },
+//     });
+// };
+
+// module.exports = { remindItemRequestDeadline };
